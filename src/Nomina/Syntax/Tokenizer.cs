cfaa107c30f104141d475734f377373f7eb8af46
Taylor Southwick <twsouthwick@outlook.com>
2020-03-11 12:45:25 -0700

    Add simple tokenizer

---
+++ src/Nomina/Syntax/Tokenizer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nomina.Syntax
+{
+    public static class Tokenizer
+    {
+        public static IEnumerable<Token> Tokenize(ReadOnlySpan<char> pattern)
+        {
+            var list = new List<Token>();
+
+
+
+            return list;
+        }
+    }
+
+    public abstract class Token
+    {
+    }
+}
---
+++ test/Nomina.Tests/TokenizerTests.cs
@@ -0,0 +1,16 @@
+﻿using Nomina.Syntax;
+using Xunit;
+
+namespace Nomina.Tests
+{
+    public class TokenizerTests
+    {
+        [Fact]
+        public void EmptyList()
+        {
+            var result = Tokenizer.Tokenize(string.Empty);
+
+            Assert.Empty(result);
+        }
+    }
+}
